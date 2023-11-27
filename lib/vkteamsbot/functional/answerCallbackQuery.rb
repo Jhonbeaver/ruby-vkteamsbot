240fa8cfa48fabbd091e60b3f22bae8496381ed4
Ivan Sodorov <Jhonbeaver@users.noreply.github.com>
2023-11-27 18:28:31 +0500

    Update answerCallbackQuery.rb

--- lib/vkteamsbot/functional/answerCallbackQuery.rb
+++ lib/vkteamsbot/functional/answerCallbackQuery.rb
@@ -3,10 +3,11 @@ require_relative '../bot.rb'
 module VKTeams
 
   class Bot
-    def answerCallbackQuery queryId, text
+    def answerCallbackQuery queryId, text, showAlert = false
       params = create_message_params '', nil, nil
       params['queryId'] = queryId
       params['text'] = text
+      params['showAlert'] = showAlert
       json_load Requests.get(API.answer_callback_query, params: params).body
     end
   end
