53ccd96351b44b6ff9167b50d04967b033a3c623
Ivan Sodorov <Jhonbeaver@users.noreply.github.com>
2023-12-01 19:47:56 +0500

    Update bot.rb

--- lib/vkteamsbot/bot.rb
+++ lib/vkteamsbot/bot.rb
@@ -46,7 +46,7 @@ module VKTeams
         events = json_load(get_events.body)
         if events and events['events'] and events['events'] != []
           last_event = events['events'].last
-          @last_event_id = last_event['eventId']
+          @last_event_id = last_event[:eventId]
           if @callback_handlers.has_key? last_event.try(:text)
             @handlers[last_event[:text]].call last_event
           elsif last_event[:type] == VKTeams::TypeEvent::CALLBACK and @callback_handlers.has_key? last_event[:data] 
